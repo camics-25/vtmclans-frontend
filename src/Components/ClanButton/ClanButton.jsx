6336e7c6c56f1a47f09d19df73c100e0d250b417
camics-25 <camilasantosrocha259@gmail.com>
2026-06-15 21:10:45 -0300

    atualizando tag de acessibilidade no botao dos cards

--- src/Components/ClanButton/ClanButton.jsx
+++ src/Components/ClanButton/ClanButton.jsx
@@ -2,10 +2,12 @@ import "./ClanButton.css";
 
 function ClanButton({ text, onClick }) {
   return (
-    <button className="clan-button" onClick={onClick}>
+    <button tabindex="-1" aria-label="botao de selecionar clã" className="clan-button" onClick={onClick}>
       {text}
     </button>
   );
 }
 
 export default ClanButton;
+
+
